7e1597d5a8354cfa2914e4eaef8360afaa35af19
enspour <enspour@gmail.com>
2023-04-13 15:13:49 +0300

    setup project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from "next/app";
+
+import "@styles/reset.css";
+import "@styles/globals.scss";
+
+export default function App({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
+}
