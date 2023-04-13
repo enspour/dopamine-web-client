75ecc9d0db00ce6bef0a7b32c9dca3e57efc33de
enspour <enspour@gmail.com>
2023-04-13 22:44:50 +0300

    added: fonts, updated: styles, tsconfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,19 @@ import type { AppProps } from "next/app";
 import "@styles/reset.css";
 import "@styles/globals.scss";
 
+import "@api/mock";
+
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({
+    weight: ["400", "500", "600", "700"],
+    subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <main className={nunito.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
