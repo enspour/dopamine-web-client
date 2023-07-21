c9afdd195db5e44c3aa9f8c40c1751eb5c3c6045
enspour <enspour@gmail.com>
2023-07-21 16:52:56 +0300

    added: redirect to login if user is not auth

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,21 +1,31 @@
 "use client";
 
+import { usePathname, useRouter } from "next/navigation";
 import { ReactNode, useState } from "react";
 
 import GuestLayout from "@components/layouts/GuestLayout/GuestLayout";
 import UserLayout from "@components/layouts/UserLayout/UserLayout";
 import UserLoaderLayout from "@components/layouts/UserLoaderLayout/UserLoaderLayout";
 
+const privatePathnames = ["/feed", "/store"];
+
 interface LayoutProps {
     children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+    const pathname = usePathname();
+    const router = useRouter();
+
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
     const callback = (success: boolean, done: () => void): void => {
         if (success) {
             setIsAuthenticated(true);
+        } else {
+            if (privatePathnames.includes(pathname)) {
+                return router.push("/login");
+            }
         }
 
         done();
