0afaa1181947239f889ca9a4a5adecc9a7300bfa
enspour <enspour@gmail.com>
2023-07-23 21:39:51 +0300

    fixed: load and update intermessages at using useLocate hook

--- src/features/internationalization/hooks/client/useLocate.ts
+++ src/features/internationalization/hooks/client/useLocate.ts
@@ -5,10 +5,11 @@ import cookies from "js-cookie";
 import { useAppDispatch, useAppSelector } from "@redux/hooks";
 import {
     selectLocate,
+    setInterMessages,
     setLocate,
 } from "@redux/slices/internationalization.slice";
 
-import { Locate } from "@features/internationalization/interfaces";
+import { Locate, getInterMessages } from "@features/internationalization";
 
 import { LOCATE_COOKIE_PATH } from "../../constants";
 
@@ -17,10 +18,14 @@ export const useLocate = () => {
 
     const dispatch = useAppDispatch();
 
-    const set = (locate: Locate) => {
+    const update = async (locate: Locate) => {
+        const interMessages = await getInterMessages(locate);
+
         dispatch(setLocate(locate));
+        dispatch(setInterMessages(interMessages));
+
         cookies.set(LOCATE_COOKIE_PATH, locate);
     };
 
-    return [locate, set];
+    return [locate, update] as const;
 };
